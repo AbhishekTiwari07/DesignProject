eed340b4e5a3714e88bd9a6fbfcb7d33926ad6f5
AbhishekTiwari07 <abhishekt2024@gmail.com>
2021-11-27 00:54:39 +0530

    Initial commit2.0

--- route/user.js
+++ route/user.js
@@ -1,9 +1,48 @@
-const router = require('express').Router();
+const User = require('../model/user')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const router = require('express').Router()
+const auth = require('../middleware/auth')
+require('dotenv').config()
 
-router.get('/', (req, res)=>{
-    res.status(200).send({
-        message: 'Working'
-    })
+router.post('/register', async (req,res)=>{
+    try{
+        const user = new User(req.body);
+        const result = await user.save();
+        res.status(200).send(result);
+    }
+    catch(e){
+        res.status(400).send({
+            message: e.message
+        })
+    }
+});
+
+router.post('/login', async (req,res)=>{
+    try{
+        const user = await User.findOne({
+            email : req.body.email
+        })
+
+        if(!user)
+            throw new Error('No User Found')
+        const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+        if(!isMatch)
+            throw new Error('Wrong Email/Password');
+
+        const token = jwt.sign({ email: user.email }, process.env.TOKEN_SECRET)
+            
+        res.status(200).send({
+            token
+        })
+
+    }
+    catch(e){
+        res.status(400).json({
+            message: e.message
+        })
+    }
 });
 
 module.exports = router;
